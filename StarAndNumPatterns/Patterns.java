ac2900c3943391d4e8f1a8478014661f40f0b168
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2025-01-11 23:07:30 +0530

    Revert "added notepad" Rvert This reverts commit 521bd5b4cdee7e84f59c296dcff2de1ae2bd6cf2.

--- StarAndNumPatterns/Patterns.java
+++ StarAndNumPatterns/Patterns.java
@@ -41,7 +41,6 @@ public class Patterns {
             System.out.println();
         }
     }
-    
     public static void Pattern5(int num){
         for(int i=0;i<num;i++){
             for(int j=0;j<num;j++){
