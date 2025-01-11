032dacac92669a2c6caa1c29889cbb694fb32c04
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2025-01-11 22:10:22 +0530

    Pattern 5

--- StarAndNumPatterns/Patterns.java
+++ StarAndNumPatterns/Patterns.java
@@ -41,4 +41,17 @@ public class Patterns {
             System.out.println();
         }
     }
+    public static void Pattern5(int num){
+        for(int i=0;i<num;i++){
+            for(int j=0;j<num;j++){
+                if(i==0 || j==0 || j==num-1 || i== num-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
